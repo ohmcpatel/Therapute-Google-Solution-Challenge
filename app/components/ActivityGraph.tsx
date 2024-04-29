b8613d7d86ae30faf8aba55c1b00e0e4694bd9a9
Pranavkrishna Suresh <85195581+pranavkrishnasuresh@users.noreply.github.com>
2024-04-29 00:16:54 -0400

    frontend updates

---
+++ app/components/ActivityGraph.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+// ActivityGraph.tsx
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface ChartData {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    borderColor: string;
+    backgroundColor: string;
+  }[];
+}
+
+interface ActivityGraphProps {
+  data: ChartData;
+}
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Activity',
+    },
+  },
+  scales: {
+    x: {
+      title: {
+        display: true,
+        text: 'Time'
+      }
+    },
+    y: {
+      title: {
+        display: true,
+        text: 'Exercises'
+      }
+    }
+  }
+};
+
+const ActivityGraph: React.FC<ActivityGraphProps> = ({ data }) => {
+  return <Line options={options} data={data} />;
+};
+
+export default ActivityGraph;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import {Button} from "@nextui-org/button"
-
+import TodayExercise from "./components/TodayExercise";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold">Hello</h1>
-      <Button>Click Me</Button>
+      <h1 className="text-red-500">Hello World</h1>
+      <TodayExercise />
     </main>
   );
 }
